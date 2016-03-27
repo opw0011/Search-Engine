4108a9b87271a39fc247815a6e117fe939c62d39
Pui Wai O <i.mpeople@gmail.com>
2016-03-27 17:33:37 +0800

    IDEA project init

---
+++ src/SE/Test.java
@@ -0,0 +1,14 @@
+package SE;
+
+/**
+ * Created by opw on 27/3/2016.
+ */
+public class Test {
+    public int test() {
+        return 123321;
+    }
+
+    public static void main(String[] args) {
+        System.out.print("hi");
+    }
+}
