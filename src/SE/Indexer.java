c06d54bcbb4b7c0e628f35c1c9d53e5884709aa5
Benny O <opw0011@gmail.com>
2016-04-07 22:59:17 +0800

    indexer fix null pointer bug

--- src/SE/Indexer.java
+++ src/SE/Indexer.java
@@ -21,8 +21,10 @@ public class Indexer {
     private static InvertedIndex titleInvertedIndex;
     private static InvertedIndex bodyInvertedIndex;
     private static PageProperty properyIndex;
-    private static ParentChildIndex PCIndex;
     private static ForwardIndex forwardIndex;
+
+    private static ParentChildIndex parentChildIndex;   // parentPageId -> {childPageIdList}
+    private static ParentChildIndex childParentIndex;   // childPageId -> {parentPageIdList}
 
     private static StopStem stopStem;
 
@@ -42,7 +44,8 @@ public class Indexer {
         titleInvertedIndex = new InvertedIndex(recman, "titleInvertedIndex");
         bodyInvertedIndex = new InvertedIndex(recman, "bodyInvertedIndex");
         properyIndex = new PageProperty(recman, "pagePropertyIndex");
-        PCIndex = new ParentChildIndex(recman, "parentChildIndex");
+        parentChildIndex = new ParentChildIndex(recman, "parentChildIndex");
+        childParentIndex = new ParentChildIndex(recman, "childParentIndex");
         forwardIndex = new ForwardIndex(recman, "forwardIndex");
 
         stopStem = new StopStem("stopwords.txt");
@@ -166,6 +169,9 @@ public class Indexer {
     // return true if page last mod date is changed -> need to crawl the page again
     public boolean pageLastModDateIsUpdated(Date newDate) throws IOException
     {
+        if(properyIndex.get(this.pageID) == null)
+            return true;
+        
         Properties p = properyIndex.get(this.pageID);
         System.out.println(newDate);
         System.out.println(p.getModDate());
@@ -176,6 +182,28 @@ public class Indexer {
     public boolean pageIsContains() throws IOException
     {
         return (urlIndex.getValue(this.url) > 0);
+    }
+
+    // insert child pages to parentChildIndex
+    public void insertChildPage(String url) throws IOException
+    {
+        int childPageId = urlIndex.getValue(url);
+        if(childPageId < 0) {
+            urlIndex.insert(url);
+            childPageId = urlIndex.getValue(url);
+        }
+        parentChildIndex.insert(this.pageID, childPageId);
+    }
+
+    // insert child pages to parentChildIndex
+    public void insertParentPage(String url) throws IOException
+    {
+        int childPageId = urlIndex.getValue(url);
+        if(childPageId < 0) {
+            urlIndex.insert(url);
+            childPageId = urlIndex.getValue(url);
+        }
+        parentChildIndex.insert(this.pageID, childPageId);
     }
 
     public void printWordMappingIndex() throws IOException
