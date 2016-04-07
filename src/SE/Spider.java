3dc64b90f65e4ba690a9d20099c60d86439e8cb9
Jacky <kwtsuiaa@gmail.com>
2016-04-08 00:03:49 +0800

    spider

--- src/SE/Spider.java
+++ src/SE/Spider.java
@@ -5,6 +5,7 @@ import org.htmlparser.util.ParserException;
 import java.io.IOException;
 import jdbm.*;
 import java.util.*;
+import java.util.concurrent.SynchronousQueue;
 
 /**
  * Created by opw on 27/3/2016.
@@ -12,15 +13,14 @@ import java.util.*;
 public class Spider {
     private static RecordManager recman;
     private static final String DB_PATH = "data/database";
-    private static int numOfPage = 0;
     private static Vector<String> DoneList = new Vector<String>();
-    private static Vector<String> TaskList = new Vector<String>();
     private static Queue<String> task = new LinkedList();
-    private static int MAXPAGE = 15;
+    private static int MAXPAGE = 5;
 
     public static void main(String[] args) {
         System.out.println("SPIDER START...");
         final long startTime = System.currentTimeMillis();
+
 
         try {
             fetch("http://www.cs.ust.hk/~dlee/4321/");
@@ -34,44 +34,61 @@ public class Spider {
     }
 
     public static void fetch(String url) throws ParserException, IOException{
-        System.out.println(url);
+        System.out.println("xxxxxxxxxxxxxxxxxxxx"+url);
         if(DoneList.size()<MAXPAGE-1)
         {
-            Crawler crawler = new Crawler(url);
-            Vector<String> links = crawler.extractLinks();
-            System.out.println(links);
-            int pagesize = crawler.getPageSize();
-            System.out.println(pagesize);
-            Date lastupdate = crawler.lastUpdate();
-            System.out.println(lastupdate);
-            Vector<String> title = crawler.extractTitle();
-            System.out.println(title);
-            Vector<String> word = crawler.extractTitle();
-            System.out.println(word);
-            Indexer indexer = new Indexer(DB_PATH,url);
 
-            for(int i=0; i<links.size(); i++)
-            {
-                task.add(links.elementAt(i));
-            }
+            Indexer indexer = new Indexer(DB_PATH,url);
+            Crawler crawler = new Crawler(url);
+
+
             if(!indexer.pageIsContains())
             {
+                Vector<String> links = crawler.extractLinks();
+                for(int i=0; i<links.size(); i++)
+                {
+                    task.add(links.elementAt(i));
+                }
+                System.out.println(links);
+                int pagesize = crawler.getPageSize();
+                System.out.println(pagesize);
+                Date lastupdate = crawler.lastUpdate();
+                System.out.println(lastupdate);
+                Vector<String> title = crawler.extractTitle();
+                System.out.println(title);
+                Vector<String> word = crawler.extractTitle();
+                System.out.println(word);
                 indexer.insertWords(word);
                 indexer.insertPageProperty(title.toString(),url,lastupdate,pagesize);
                 DoneList.add(url);
-                task.remove();
             }
             else
             {
+                Date lastupdate = crawler.lastUpdate();
+                System.out.println("last updaaaaate:"+lastupdate);
                 if(indexer.pageLastModDateIsUpdated(lastupdate))
                 {
+                    Vector<String> links = crawler.extractLinks();
+                    for(int i=0; i<links.size(); i++)
+                    {
+                        task.add(links.elementAt(i));
+                    }
+                    System.out.println(links);
+                    int pagesize = crawler.getPageSize();
+                    System.out.println(pagesize);
+                    Vector<String> title = crawler.extractTitle();
+                    System.out.println(title);
+                    Vector<String> word = crawler.extractTitle();
+                    System.out.println(word);
                     indexer.insertWords(word);
                     indexer.insertPageProperty(title.toString(),url,lastupdate,pagesize);
                     DoneList.add(url);
-                    task.remove();
                 }
             }
+            task.remove();
             indexer.finalize();
+
+
           try{
               fetch(task.peek());
           }catch (Exception e){
